2885854634bee320c0724279682174f72d7e3154
Ong Thao <hellopanmoo@gmail.com>
2019-03-30 11:46:14 -0700

    Test

---
+++ DemoCSC131/src/MyClass.java
@@ -0,0 +1,11 @@
+
+public class MyClass {
+
+<<<<<<< HEAD
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+	}
+
+=======
+>>>>>>> branch 'master' of https://gitlab.ecs.csus.edu/thaoo/csc131-project.git
+}
